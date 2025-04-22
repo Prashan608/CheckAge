ad46b4d71cab73fd5cfe679ed42a25464cbad600
Prashan608 <prashantmish932@gmail.com>
2025-04-22 11:24:42 +0530

    File Added Sucessfully

---
+++ Sorting/BubbleSortin.js
@@ -0,0 +1,17 @@
+function bubblesort(arr){
+    let n = arr.length;
+  for(let i = 0; i<n;i++){
+    for(let j = 0; j<n-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            let temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        } 
+    }
+  }
+  return arr;
+}
+
+let arr = [3,5,0,9,8];
+let res = bubblesort(arr);
+console.log(res);
