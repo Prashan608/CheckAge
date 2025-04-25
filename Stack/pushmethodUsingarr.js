51b1c286c563096017655b94f548edb6f4f02036
Prashan608 <prashantmish932@gmail.com>
2025-04-26 00:00:37 +0530

    push Mehod file Added Successfully

---
+++ Stack/pushmethodUsingarr.js
@@ -0,0 +1,20 @@
+let max = 5;
+let arr = Array(max).fill(0);
+let top = -1;
+function push(x){
+    if(top>=max-1){
+        console.log("Stack is full");
+        return;
+    }
+    else{
+        arr[++top] = x;
+        console.log(x+" is Pushed to stack");
+    }
+}
+push(5);
+push(10);
+push(15);
+push(20);
+push(25);
+// it give stackover flow error
+push(30)
