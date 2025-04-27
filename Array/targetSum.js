a36477d09e1f4235735c6be6cbed2e1b1b6b386d
Prashan608 <prashantmish932@gmail.com>
2025-04-27 17:47:47 +0530

    Two pointer file Added Successfully

---
+++ Array/targetSum.js
@@ -0,0 +1,22 @@
+
+function findtarget(arr,k){
+ arr.sort((a,b)=>a-b);
+ let n = arr.length-1;
+ let l = 0 ,r = n;
+ while(l<r){
+  let sum = arr[l]+arr[r];
+  if(sum==k){
+    return true;
+  }
+  else if(sum>k){
+    r--;
+  }
+  else{
+    l++;
+  }
+ }
+ return false;
+}
+let arr = [7,4,9,6,21,8,11,17];
+let k = 18;
+console.log(findtarget(arr,k)?"Found":"Not found");
