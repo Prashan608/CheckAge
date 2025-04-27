d7a7a735756907726410187a24728f59d66fa374
Prashan608 <prashantmish932@gmail.com>
2025-04-27 18:43:00 +0530

    file Added Successfully

--- Array/findGreaterElement.js
+++ Array/findGreaterElement.js
@@ -2,14 +2,14 @@ function findGreater(arr){
     let ans = -1;
   for(let i = 1; i<arr.length;i++){
     let left = 0;
-    for(let j = i-1;j>=0;j--){
+    for(let j = i-1;j>=0;j--){  // left side bigger
         if(arr[j]>arr[i]){
             left = 1;
             break;
         }
     }
     let right = 0;
-    for(let j = i+1;j<arr.length;j++){
+    for(let j = i+1;j<arr.length;j++){ // right side smaller
         if(arr[j]<arr[i]){
             right = 1;
             break;
